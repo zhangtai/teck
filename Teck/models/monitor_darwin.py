f2b61f48e35e994864298a0a2b42358d7c92a0f0
Tai Zhang <tai.zhang@outlook.com>
2022-08-19 12:48:21 +0800

    Add darwin monitor

---
+++ Teck/models/monitor_darwin.py
@@ -0,0 +1,69 @@
+import logging
+import platform
+import time
+
+from StreamDeck.Transport.Transport import TransportError
+
+from .teck import Teck
+
+if platform.system() == "Darwin":
+    from AppKit import NSWorkspace
+
+    def get_active_application_name() -> str:
+        active_app_name = (
+            NSWorkspace.sharedWorkspace()
+            .activeApplication()
+            .get("NSApplicationBundleIdentifier")
+        )
+        return active_app_name
+
+if platform.system() == "Windows":
+    import psutil
+    from win32gui import GetForegroundWindow, GetWindowText
+    from win32process import GetWindowThreadProcessId
+
+    def get_active_application_name() -> str:
+        new_pid = GetWindowThreadProcessId(GetForegroundWindow())
+        try:
+            active_app_name = psutil.Process(new_pid[-1]).name()
+        except (psutil.NoSuchProcess, ValueError):
+            logger.warning("Can't get process, ignore")
+            active_app_name = "explorer.exe"
+        return active_app_name
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %f(levelname)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class Monitor(object):
+    """Class to monitor window switch and trigger Teck instance page switching."""
+    def __init__(self) -> None:
+        self.teck = Teck()
+        self.active_application = "system"
+
+    def start(self) -> None:
+        while True:
+            time.sleep(3)
+            self.teck.refresh_button_text_image()
+            active_app_name = get_active_application_name()
+            new_page_name = (
+                active_app_name
+                if active_app_name in self.teck.config.pages
+                else list(self.teck.config.pages.keys())[0]
+            )
+            if new_page_name != self.teck.active_page:
+                self.teck.blank_page()
+                logger.info("Switching to new page: %s", new_page_name)
+                self.teck.active_page = new_page_name
+                try:
+                    self.teck.refresh_page()
+                except TransportError:
+                    logger.warning(
+                        "No HID device, sleep %s seconds and retry",
+                        self.teck.config.retry_interval,
+                    )
+                    time.sleep(self.teck.config.retry_interval)
+                    self.teck.refresh_page()
