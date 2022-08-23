2532a07b24c1b9377fdeb76821fa12db5dc20280
Tai Zhang <tai.zhang@outlook.com>
2022-08-23 06:43:13 +0000

    Move image_providers to config file

--- config/settings.py
+++ config/settings.py
@@ -9,22 +9,6 @@ from Teck.utils.images import generate_button_function_image, open_image_as_png
 
 DEFAULT_FONT = "assets/fonts/Roboto-Regular.ttf"
 
-IMAGE_PROVIDERS = {
-    "file": {
-        "path": "assets/pages"
-    },
-    "fontawesome": {
-        "path": "assets/vendor/fontawesome/svgs",
-        "inverted": True
-    },
-    "fluentui-emoji": {
-        "path": "assets/vendor/fluentui-emoji/assets"
-    },
-    "google-cloud-icons": {
-        "path": "assets/vendor/google-cloud-icons"
-    },
-}
-
 
 @dataclass
 class ButtonImage:
@@ -73,6 +57,7 @@ def get_deck_config(
     config_file: str = f"config/{platform.system()}.demo.yaml",
 ) -> DeckConfig:
     config_dict = yaml.load(Path(config_file).read_text(encoding="utf-8"), Loader=yaml.FullLoader)
+    image_providers = config_dict["image_providers"]
     configs = DeckConfig(
         refresh_interval=config_dict.get("refresh_interval", 3),
         retry_interval=config_dict.get("retry_interval", 5),
@@ -95,12 +80,13 @@ def get_deck_config(
                 button_image.pil_image = generate_button_function_image(button["image"]["function"])
                 button_image.function = button["image"]["function"]
             else:
+                assert image_providers is not None
                 if button["image"]["provider"] == "file":
-                    image_base_path = IMAGE_PROVIDERS.get(button["image"]["provider"]).get("path") + "/" + page_name
+                    image_base_path = image_providers.get(button["image"]["provider"]).get("path") + "/" + page_name
                 else:
-                    image_base_path = IMAGE_PROVIDERS.get(button["image"]["provider"]).get("path")
+                    image_base_path = image_providers.get(button["image"]["provider"]).get("path")
                 button_image.pil_image = open_image_as_png(f"{image_base_path}/{button['image']['path']}")
-                if IMAGE_PROVIDERS.get(button["image"]["provider"]).get("inverted", False):
+                if image_providers.get(button["image"]["provider"]).get("inverted", False):
                     button_image.pil_image = ImageOps.invert(button_image.pil_image)
                 button_image.path = button['image']['path']
 
